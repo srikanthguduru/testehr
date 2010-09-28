57fd3e98b2d0b136a054fac18034f0400e278d55
kd7tck <kd7tck@87096503-a0ab-fd47-48a3-141c8915c89c>
2010-09-28 03:04:55 +0000

    init

---
+++ trunk/src/edu/utdallas/android/foundation/foundation.java
@@ -0,0 +1,13 @@
+package edu.utdallas.android.foundation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class foundation extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
