6981619f56bef7379deab4e379595e65c96a0f8d
kd7tck <kd7tck@87096503-a0ab-fd47-48a3-141c8915c89c>
2010-10-27 19:59:50 +0000

    added vitals class in core package.

---
+++ trunk/src/edu/utdallas/hf/core/Vitals.java
@@ -0,0 +1,22 @@
+package edu.utdallas.hf.core;
+
+public class Vitals {
+	private float temperature = 0.0f;
+	private float bmi = 0.0f;
+	
+	
+	public void setTemperature(float temperature) {
+		this.temperature = temperature;
+	}
+	public float getTemperature() {
+		return temperature;
+	}
+	public void setBmi(float bmi) {
+		this.bmi = bmi;
+	}
+	public float getBmi() {
+		return bmi;
+	}
+	
+	
+}
