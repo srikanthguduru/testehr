d84ae95cf76d9d5a4f0f02aaa1b8c36cadefee3c
bnv061000@gmail.com <bnv061000@gmail.com@87096503-a0ab-fd47-48a3-141c8915c89c>
2010-11-04 00:43:47 +0000

    patient notes added to Connection.java

--- trunk/src/edu/utdallas/hf/db/Connection.java
+++ trunk/src/edu/utdallas/hf/db/Connection.java
@@ -21,6 +21,7 @@ import edu.utdallas.hf.commons.DateUtil;
 import edu.utdallas.hf.core.Medication;
 import edu.utdallas.hf.core.Patient;
 import edu.utdallas.hf.core.Vitals;
+import edu.utdallas.hf.core.Note;
 
 
 public class Connection
@@ -273,5 +274,61 @@ public class Connection
 		return mList;
 
     }
+    
+    public ArrayList<Note> getPatientNote(int pid)
+    {
+    	ArrayList<Note> pNotes = new ArrayList<Note>();
+    	Note notes;
+    	
+    	String cmd = "patientNotes";
+    	Log.i("Connection", "Sending message");
+    	
+		try
+		{
+			connect();
+			//Encode the string combination into a url to send to the php page
+			String data = URLEncoder.encode("pid", "UTF-8") + "=" +
+				URLEncoder.encode(""+pid, "UTF-8") + "&" +
+				URLEncoder.encode("cmd", "UTF-8") + "=" +
+				URLEncoder.encode(cmd, "UTF-8");
+			
+			Log.i("Connection", "Sending message: " + data);
+
+		    OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+            wr.write(data);
+            wr.flush();
+            Log.i("Connection", "Sending message to web");
+			String buffer;
+			
+			BufferedReader rd = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			while ((buffer = rd.readLine()) != null)
+			{
+				
+				Log.i("Connection", "Retrieving message buffer is " + buffer);
+				String[] bufferString = buffer.split(",");
+				
+				//0 >> 3
+				//Log.e("Connection", "Drug: "+bufferString[2]);
+				Calendar noteDate = new GregorianCalendar();
+				noteDate = DateUtil.parseDateString(bufferString[3]);
+				notes = new Note(Integer.parseInt(bufferString[0]), 
+						bufferString[1],
+						bufferString[2],
+						noteDate
+						);
+				pNotes.add(notes);
+			}
+			
+			wr.close();
+			rd.close();
+    	
+    	
+		}
+		catch (Exception blah)
+		{
+			Log.i("Connection", "Exception: " + blah);
+		}
+		return pNotes;
+   }
     
 }
