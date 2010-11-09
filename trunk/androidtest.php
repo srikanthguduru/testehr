ffd26439b79fdb1fb0fd4859c44191fb7b8148df
bnv061000@gmail.com <bnv061000@gmail.com@87096503-a0ab-fd47-48a3-141c8915c89c>
2010-11-09 01:54:54 +0000

    PHP Side for update and create patient note. Still have yet to add the new doctor schedule query, but its already made and ready.

--- trunk/androidtest.php
+++ trunk/androidtest.php
@@ -20,7 +20,15 @@ if($_POST[cmd] == "login") {
 			print "Fail.";
 	}
 
-}elseif($_POST[cmd] == "patientList") {
+} elseif($_POST[cmd] == "hasLoggedIn") {
+	$sql = "SELECT id from users WHERE username='" . $_POST[username] ."'";
+	$user_results = DoMysqlQuery("openemr", $sql);
+	
+	foreach($sql_results as $res) {
+		print $res[id] . "\n";
+	}
+}
+elseif($_POST[cmd] == "patientList") {
 
 	$sql = "SELECT fname, lname, dob, pid FROM openemr.patient_data";
 	$sql_results = DoMysqlQuery("openemr", $sql);
@@ -51,10 +59,34 @@ elseif($_POST[cmd] == "medicationList") {
 elseif($_POST[cmd] == "doctorSchedule") {
 	$sql = "SELECT pc_aid, pc_title, pc_time FROM openemr.openemr_postcalendar_events where pc_aid = '" . $_POST[pc_aid] . "'";
 	$sql_results = DoMysqlQuery("openemr", $sql);
-	
+	 
 	foreach($sql_results as $res) {
 		print $res[pc_aid] . ',' . $res[pc_title] . ',' . $res[pc_time] . "\n";
 	}
+}
+elseif($_POST[cmd] == "patientNotes") {
+	$sql = "select id, title, body, date from openemr.pnotes where pid = '" . $_POST[pid] . "'";
+	$sql_results = DoMysqlQuery("openemr", $sql);
+	
+	foreach($sql_results as $res) {
+		print $res[id] . ',' . $res[title] . ',' . $res[body] . ',' . $res[date] . "\n";
+	}
+}
+elseif ($_POST[cmd] == "updatePatientNote") {
+	$sql = "update pnotes set body = '" . $_POST[msg] . "' where id = '" . $_POST[id] . "'";
+	DoMysqlQuery("openemr", $sql);
+	print "Hi";
+}
+elseif ($_POST[cmd] == "createPatientNote") {
+	$sql = "select count(*) from pnotes";
+	$sql_results = DoMysqlQuery("openemr", $sql);
+	$res = $sql_results;
+	$res = $res + 1;
+	$sql = "insert into pnotes values('" . $res . "', '" . NOW() . 
+	"', '" . $_POST[msg] . "', '1', 'admin', NULL, NULL, NULL, '" . $_POST[title] . "', NULL, '0'";
+	DoMysqlQuery("openemr", $sql);
+	
+	
 }
 else {
 	print "No.";
